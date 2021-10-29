d186100de783f93809bad70a27e98f0fe9a87655
chongks0611 <veralynchong@gmail.com>
2021-10-29 08:26:56 +0000

    header commit

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,17 +2,11 @@
     export let title;
 </script>
 
-<h1 class="card">{title}</h1>
+<h1>{title}</h1>
 
 <style>
 	h1 {
 		background-color: lightskyblue;
-	}
-
-	.card {
-		padding: 2px 16px;
-		margin: 12px 4px;
-		border-radius: 5px;
 		text-align: center;
 	}
 </style>
