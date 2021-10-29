e9b63c928cf2349ac3dbafbe105b0f6938188f38
chongks0611 <veralynchong@gmail.com>
2021-10-29 08:48:41 +0000

    section commit

--- src/lib/Section.svelte
+++ src/lib/Section.svelte
@@ -5,10 +5,17 @@
     export let item3;
 </script>
 
-<div>
+<div class="card">
     <h3>{title}</h3>
     <p>{item1}</p>
     <p>{item2}</p>
     <p>{item3}</p>
 </div>
 
+<style>
+	.card {
+		background-color: #F9F9F9;
+		text-align: center;
+		padding: 50px;
+	}
+</style>
