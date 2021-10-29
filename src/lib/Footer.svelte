926020480c8d9369c922b6ee131a2ab20e4f2f34
chongks0611 <veralynchong@gmail.com>
2021-10-29 09:15:28 +0000

    style commit

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,14 +2,19 @@
     export let gitHub;
 </script>
 
-
+<div>
 <a href = {gitHub}> GitHub repository</a>
 <p>My Biography $copy All rights reserved.</p>
+</div>
 
 <style>
- a {
+ div {
     background-color: #C490E4;
     text-align: center;
     padding: 20px;
+}
+
+a {
+    color: cyan;
 }
 </style>
