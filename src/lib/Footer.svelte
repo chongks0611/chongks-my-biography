a5f161c89d5000d56da73b0d8a0e934e7b57d721
chongks0611 <veralynchong@gmail.com>
2021-10-29 09:13:02 +0000

    style commit

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,4 +2,14 @@
     export let gitHub;
 </script>
 
-<a href = {gitHub}> GitHub </a>
+
+<a href = {gitHub}> GitHub repository</a>
+<p>My Biography $copy All rights reserved.</p>
+
+<style>
+ a {
+    background-color: #C490E4;
+    text-align: center;
+    padding: 20px;
+}
+</style>
--- src/lib/Personality.svelte
+++ src/lib/Personality.svelte
@@ -5,9 +5,18 @@
     export let item3;
 </script>
 
-<div>
+<div class="card">
     <h3>{title}</h3>
     <p>{item1}</p>
     <p>{item2}</p>
     <p>{item3}</p>
 </div>
+
+<style>
+	.card {
+		background-color: #F6C6EA;
+		text-align: center;
+		padding: 15px;
+        margin: 15px 80px;
+	}
+</style>
